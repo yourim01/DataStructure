9dd95e74ff7b12395e414f7508cd30c731d2f460
yourim01 <162237840+yourim01@users.noreply.github.com>
2024-05-27 23:54:23 +0900

    이진 탐색 트리 7_4

---
+++ bst.c
@@ -0,0 +1,83 @@
+#include<stdio.h> 
+#include<stdlib.h> 
+#include "node.h"
+#include "bst.h"
+
+treeNode* searchBST(treeNode* root, element x) {
+	treeNode* p;
+	p = root;
+	while (p != NULL) {
+		if (x < p->key) p = p->left;
+		else if (x == p->key) return p;
+		else p = p->right;
+	}
+	printf("\n 찾는 키가 없습니다!");
+	return p;
+}
+
+treeNode* insertBSTNode(treeNode* p, element x) {
+	treeNode* newNode;
+	if (p == NULL) {
+		newNode = (treeNode*)malloc(sizeof(treeNode));
+		newNode->key = x;
+		newNode->left = NULL;
+		newNode->right = NULL;
+		return newNode;
+	}
+	else if (x < p->key)  p->left = insertBSTNode(p->left, x);
+	else if (x > p->key)  p->right = insertBSTNode(p->right, x);
+	else  printf("\n 이미 같은 키가 있습니다! \n");
+
+	return p;
+}
+
+void deleteBSTNode(treeNode* root, element key) {
+	treeNode* parent, * p, * succ, * succ_parent;
+	treeNode* child;
+
+	parent = NULL;
+	p = root;
+	while ((p != NULL) && (p->key != key)) {  // 삭제할 노드의 위치 탐색
+		parent = p;
+		if (key < p->key) p = p->left;
+		else p = p->right;
+	}
+
+	if (p == NULL) {
+		printf("\n 찾는 키가 이진 트리에 없습니다!!");
+		return;
+	}
+
+	if ((p->left == NULL) && (p->right == NULL)) {
+		if (parent != NULL) {
+			if (parent->left == p) parent->left = NULL;
+			else parent->right = NULL;
+		}
+		else root = NULL;
+	}
+
+	else if ((p->left == NULL) || (p->right == NULL)) {
+		if (p->left != NULL) child = p->left;
+		else child = p->right;
+
+		if (parent != NULL) {
+			if (parent->left == p) parent->left = child;
+			else parent->right = child;
+		}
+		else root = child;
+	}
+
+	else {
+		succ_parent = p;
+		succ = p->left;
+		while (succ->right != NULL) { 
+			succ_parent = succ;
+			succ = succ->right;
+		}
+		if (succ_parent->left == succ)  succ_parent->left = succ->left;
+		else succ_parent->right = succ->left;
+		p->key = succ->key;
+		p = succ;
+	}
+	free(p);
+}
---
+++ node.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "node.h"
+
+void displayInorder(treeNode* root) {
+	if (root) {
+		displayInorder(root->left);
+		printf("%c_", root->key);
+		displayInorder(root->right);
+	}
+}
